26d3009563b49366767c32bea207f9793a413400
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-10-11 18:37:08 +0900

    simple h function #1

--- packages/runtime-core/index.ts
+++ packages/runtime-core/index.ts
@@ -1,2 +1,3 @@
 export type { RendererOptions } from "./renderer";
 export { createRenderer } from "./renderer";
+export { h } from "./h"
--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,8 +1,13 @@
 // NOTE: renderロジックのみを持つオブジェクトを生成するためのファクトリ関数を実装する
 //       Node(DOMに限らず)を扱うオブジェクトはfactory関数の引数として受け取るようにする
 
+import { VNode } from "./vnode";
+
 export interface RendererOptions<HostNode = RendererNode> {
+  createElement(type: string): HostNode;
+  createText(text: string): HostNode;
   setElementText(node: HostNode, text: string): void;
+  insert(child: HostNode, parent: HostNode, anchor?: HostNode | null): void;
 }
 
 export interface RendererNode {
@@ -18,14 +23,27 @@ export type RootRenderFunction<HostElement = RendererElement> = (
 
 /**
  * renderロジックのみを持つオブジェクトを生成するためのファクトリ関数
- * @param {RendererOptions}options 
+ * @param {RendererOptions}options
  * @returns {RootRenderFunction}
  */
 export function createRenderer(options: RendererOptions) {
-  const { setElementText: hostSetElementText } = options;
+  const { createElement:hostCreateElement, createText:hostCreateText, insert:hostInsert } = options;
 
-  const render: RootRenderFunction = (message, container) => {
-    hostSetElementText(container, message);
+  function renderVNode(vnode: VNode | string) {
+    if(typeof vnode === "string") return hostCreateText(vnode);
+    const el = hostCreateElement(vnode.type);
+
+    for(let child of vnode.children) {
+      const childEl = renderVNode(child);
+      hostInsert(childEl, el)
+    }
+
+    return el
+  }
+
+  const render: RootRenderFunction = (vnode, container) => {
+    const el = renderVNode(vnode);
+    hostInsert(el, container)
   };
 
   return { render };
